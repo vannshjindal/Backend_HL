b85b4cea6bc8c369d3a277dee5c2154cd88ee9ef
Vansh Jindal <136730732+vannshjindal@users.noreply.github.com>
2025-03-11 15:59:57 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,9 @@
+function sayHello() {
+    console.log("Hello after 5 seconds!");
+  }
+  
+  setTimeout(sayHello, 0); 
+
+const multiply = (a, b) => a * b;
+
+console.log(multiply(4, 2)); 
--- callback.js
+++ callback.js
@@ -1,6 +1,6 @@
 function sayHello() {
-    console.log("Hello after 2 seconds!");
+    console.log("Hello after 5 seconds!");
   }
   
-  setTimeout(sayHello, 2000); 
+  setTimeout(sayHello, 5000); 
   console.log("Hello This is Vansh Jindal") // As told today this was executed first by CPU and then after 5 seconds the above function printed
